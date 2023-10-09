3bf8cb5f73791a433ff5c91eaa41b4f25e4eda21
AZ0228 <jbliu88@gmail.com>
2023-10-09 15:28:37 -0400

    added mobile support

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
     const inputElement = document.getElementById('amount');
 
-    inputElement.addEventListener('keydown', (e) => {
-        console.log(1);
-        // Ensure pressed key is a number or backspace
-        if ((e.keyCode >= 48 && e.keyCode <= 57) || e.keyCode === 8) {
-            let currentValue = parseFloat(inputElement.value.replace('$', '')).toFixed(2);
-            let newStr;
-    
-            if (e.keyCode === 8) { // Handle backspace
-                if(inputElement.value === "$0.00"){
-                    console.log(0);
-                    newStr = "0";
-                } else {
-                    newStr = (currentValue * 10).toString().slice(0, -1);
-                }
-            } else { // Handle number press
-                let pressedNumber = e.key;
-                newStr = currentValue.replace('.', '') + pressedNumber;
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+    if(isMobile){
+        inputElement.addEventListener('input', (e) => {
+            let newValue = e.target.value.replace('$', '');
+        
+            // Check if the new value is a valid number
+            if (!isNaN(parseFloat(newValue)) && isFinite(newValue)) {
+                let formattedValue = parseFloat(newValue).toFixed(2);
+                e.target.value = `$${formattedValue}`;
+            } else {
+                // If not a valid number, reset to default
+                e.target.value = "$0.00";
             }
+        });
+    } else {
+        inputElement.addEventListener('keydown', (e) => {
+            console.log(1);
+            // Ensure pressed key is a number or backspace
+            if ((e.keyCode >= 48 && e.keyCode <= 57) || e.keyCode === 8) {
+                let currentValue = parseFloat(inputElement.value.replace('$', '')).toFixed(2);
+                let newStr;
+        
+                if (e.keyCode === 8) { // Handle backspace
+                    if(inputElement.value === "$0.00"){
+                        console.log(0);
+                        newStr = "0";
+                    } else {
+                        newStr = (currentValue * 10).toString().slice(0, -1);
+                    }
+                } else { // Handle number press
+                    let pressedNumber = e.key;
+                    newStr = currentValue.replace('.', '') + pressedNumber;
+                }
+        
+                let newNumber = parseInt(newStr, 10) / 100;
+                inputElement.value = `$${newNumber.toFixed(2)}`;
+            }
+            e.preventDefault(); // Prevent any default behavior
+        });
     
-            let newNumber = parseInt(newStr, 10) / 100;
-            inputElement.value = `$${newNumber.toFixed(2)}`;
-        }
-        e.preventDefault(); // Prevent any default behavior
-    });
+    }
+
     let tips = qsa('.tip-percent');
     console.log(tips);
     tips.forEach(tip => {
