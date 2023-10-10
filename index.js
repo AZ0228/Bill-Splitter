643c9e784be2e4aa6d8d86a628cb251e910345d7
AZ0228 <jbliu88@gmail.com>
2023-10-09 23:55:19 -0400

    new

--- index.js
+++ index.js
@@ -57,10 +57,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let tips = qsa('.tip-percent');
     let customTip = id('customTip');
-    console.log(tips);
     tips.forEach(tip => {
         tip.addEventListener('click', (e) => {
-            console.log(e.target)
             if(e.target.classList.contains('selected')){
                 e.target.classList.remove('selected');
                 customTip.readOnly = false;
@@ -74,9 +72,125 @@ document.addEventListener('DOMContentLoaded', () => {
             customTip.readOnly = true;
         });
     });
-});
 
+    let inputs = qsa('input');
+    inputs.forEach(input => {
+        input.addEventListener('input', (e) => {checkInput(e)})
+    });
+    id('add').addEventListener('click', (e) => {checkInput(e)});
+    id('minus').addEventListener('click', (e) => {checkInput(e)});
 
+}); 
+
+function addItem(){
+    let input = qs(".add-prices").querySelector('input');
+    let price = input.value;
+    if(parseInt(price) === 0){
+        return;
+    }
+    price = parseInt(price).toFixed(2).toString();
+    price = "$" + price;
+    let items = qs(".new-person").querySelector('.items');
+    let itemElement = document.createElement('p');
+    itemElement.innerHTML = price;
+    items.appendChild(itemElement);
+}
+
+function minusItem(){
+    let items = qs(".new-person").querySelector('.items');
+    let itemElement = items.lastChild;
+    items.removeChild(itemElement);
+}
+
+function parsePerson(){
+    let person = {};
+    let name = id('new-name').value;
+    let items = qs(".new-person").querySelector('.items');
+    person.name = name;
+    person.items = items;
+    createNewPerson(person);
+    blurToggle()
+}
+
+function createNewPerson(person){
+    let personElement = document.createElement('div');
+    let total = parseFloat(id('amount').value.replace('$', ''));
+    let totalTax = parseFloat(id('tax').value.replace('$', ''));
+    let totalTip = parseFloat(id('tip').value.replace('$', ''));
+
+    let subtotal = 0;
+    let tax = 0;
+    let tip = 0;
+    let totalOwed = 0;
+    for(item of person.items.children){
+        subtotal += parseFloat(item.innerHTML.replace('$', ''));
+    }
+    let ratio = subtotal / total;
+    tax = (ratio * 0.07).toFixed(2);
+    personElement.classList.add('person');
+    personElement.innerHTML = `
+            <div class="top">
+                <h3>${person.name}</h3>
+                <h3 class="accent">$500.00</h3>
+            </div>
+            <div class="item-container">
+                <div class="items">
+                </div>
+                <div class="total">
+                        <div class="total-item">
+                            <p>Subtotal</p>
+                            <h4 id="grab-subtitle">$0.00</h4>    
+                        </div>
+                        <div class="total-item">
+                            <p>Tax</p>
+                            <h4 id="grab-tax">$0.00</h4>    
+                        </div>
+                        <div class="total-item">
+                            <p>Tip</p>
+                            <h4 id="grab-tip">$0.00</h4>    
+                        </div>
+                </div>  
+            </div>
+
+    `;
+}
+
+function checkInput(event){
+    let empty = false;
+    let parent = event.target.parentNode;
+    while(parent.tagName != "SECTION"){
+        parent = parent.parentNode;
+    }
+    let inputs = parent.querySelectorAll('input');
+    inputs.forEach(input => {
+        if(input.name === "prices"){
+            let itemsElement = parent.querySelector('.items');
+            if(itemsElement.children.length === 0){
+                empty = true;
+            }
+        } else{
+            if(input.value === ""){
+                empty = true;
+            }
+        }
+    });
+    console.log(parent);
+    if(empty){
+        console.log(parent.querySelector(".validation"))
+        parent.querySelector(".validation").classList.remove('active');
+    } else {
+        console.log(parent.querySelector(".validation"))
+        parent.querySelector(".validation").classList.add('active');
+    }
+}
+
+function blurToggle(){
+    let blur = qs('.blur');
+    let newPerson = qs(".new-person");
+    blur.classList.toggle('active');
+    newPerson.classList.toggle('active');
+    console.log("blur");
+}
 
 function qsa(selector) {
     return document.querySelectorAll(selector);
